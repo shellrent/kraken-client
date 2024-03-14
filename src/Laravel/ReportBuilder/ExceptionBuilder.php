606f8260b0ca1710d63c6d0a43ed5f0c598faeff
Armando Caprio <armando@shellrent.com>
2024-03-14 15:12:57 +0100

    Implementato meccanismo per cli o per web

--- src/Laravel/ReportBuilder/ExceptionBuilder.php
+++ src/Laravel/ReportBuilder/ExceptionBuilder.php
@@ -10,9 +10,7 @@ use Shellrent\KrakenClient\ReportBuilder;
 use Throwable;
 
 class ExceptionBuilder extends GenericBuilder {
-	protected function buildReport( Throwable $e ): ReportBuilder {
-		$report = ReportBuilder::createFromException( $e, config('kraken.exception_report_type') );
-		
+	protected function addHttpRequestData( ReportBuilder $report ) {
 		$report->addExtraInfo( 'session', Session::all() );
 		$report->addExtraInfo( 'cookie', Cookie::get() );
 		$report->addExtraInfo( 'server', Request::server());
@@ -20,11 +18,6 @@ class ExceptionBuilder extends GenericBuilder {
 		$report->addExtraInfo( 'requestUri', Request::getRequestUri() );
 		
 		$report->addExtraInfo( 'header', Request::header() );
-		
-		$module = $this->getModule();
-		if( $module ) {
-			$report->setModule( $module );
-		}
 		
 		$query = Request::query();
 		if( !empty( $query ) ) {
@@ -35,6 +28,46 @@ class ExceptionBuilder extends GenericBuilder {
 		if( !empty( $post ) ) {
 			$report->addExtraInfo( 'post', $post );
 		}
+	}
+	
+	protected function addCliData( ReportBuilder $report ) {
+		$report->addExtraInfo( 'command', implode( ' ', Request::server( 'argv' ) ) );
+		
+		$report->addExtraInfo( 'server', [
+			'REQUEST_TIME' => Request::server( 'REQUEST_TIME' ),
+			'SHELL' => Request::server( 'SHELL' ),
+			'LANGUAGE' => Request::server( 'LANGUAGE' ),
+			'LANG' => Request::server( 'LANG' ),
+			'XDG_SESSION_TYPE' => Request::server( 'XDG_SESSION_TYPE' ),
+			'XDG_SESSION_CLASS' => Request::server( 'XDG_SESSION_CLASS' ),
+			'TERM' => Request::server( 'TERM' ),
+			'USER' => Request::server( 'USER' ),
+			'XDG_SESSION_ID' => Request::server( 'XDG_SESSION_ID' ),
+			'XDG_RUNTIME_DIR' => Request::server( 'XDG_RUNTIME_DIR' ),
+			'PS1' => Request::server( 'PS1' ),
+			'PATH' => Request::server( 'PATH' ),
+			'_' => Request::server( '_' ),
+			'PHP_SELF' => Request::server( 'PHP_SELF' ),
+			'PATH_TRANSLATED' => Request::server( 'PATH_TRANSLATED' ),
+			'argv' => Request::server( 'argv' ),
+			'argc' => Request::server( 'argc' ),
+		]);
+	}
+	
+	protected function buildReport( Throwable $e ): ReportBuilder {
+		$report = ReportBuilder::createFromException( $e, config('kraken.exception_report_type') );
+		
+		if( App::runningInConsole() ) {
+			$this->addCliData( $report );
+		} else {
+			$this->addHttpRequestData( $report );
+		}
+		
+		$module = $this->getModule();
+		if( $module ) {
+			$report->setModule( $module );
+		}
+		
 		
 		return $report;
 	}
