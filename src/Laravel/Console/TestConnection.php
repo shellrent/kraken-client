28c550af2b74490f0fdc0127998c600631b60013
Armando Caprio <armando@shellrent.com>
2024-03-18 12:12:59 +0100

    Completato comando test configurazione laravel

--- src/Laravel/Console/TestConnection.php
+++ src/Laravel/Console/TestConnection.php
@@ -12,7 +12,24 @@ class TestConnection extends Command {
 	
 	public function handle() {
 		$result = json_decode( KrakenClient::testConnection()->getBody() );
+		$reportTypes = $result->data->types;
 		
-		$this->info( $result->data );
+		$this->info( $result->data->message );
+		
+		$exceptionType = config( 'kraken.exception_report_type' );
+		if( in_array( $exceptionType, $reportTypes ) ) {
+			$this->info( sprintf( 'Exception type "%s" successfully configured', $exceptionType ) );
+		} else {
+			$this->error( sprintf( 'Exception type "%s" not configured', $exceptionType ) );
+		}
+		
+		$logType = config('kraken.log_report_type' );
+		if( in_array( $logType, $reportTypes ) ) {
+			$this->info( sprintf( 'Log type "%s" successfully configured', $logType ) );
+		} else {
+			$this->error( sprintf( 'Log type "%s" not configured', $logType ) );
+		}
+		
+		$this->line( 'Available report types: ' . implode( ' - ', $reportTypes ) );
 	}
 }
